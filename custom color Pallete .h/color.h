eab2960f9e9ec972bcff1de28b0bba0b41eb4476
sandip <sp7354444@gmail.com>
2025-09-06 13:55:53 +0545

    ''

---
+++ custom color Pallete .h/color.h
@@ -0,0 +1,64 @@
+// === REDS & PINKS ===
+
+#define COLOR_DIMGRAY (Color){ 105, 105, 105, 255 }
+#define COLOR_DIMGREY (Color){ 105, 105, 105, 255 }
+#define COLOR_GRAY (Color){ 128, 128, 128, 255 }
+#define COLOR_GREY (Color){ 128, 128, 128, 255 }
+#define COLOR_DARKGRAY (Color){ 169, 169, 169, 255 }
+#define COLOR_DARKGREY (Color){ 169, 169, 169, 255 }
+#define COLOR_SILVER (Color){ 192, 192, 192, 255 }
+#define COLOR_LIGHTGRAY (Color){ 211, 211, 211, 255 }
+#define COLOR_LIGHTGREY (Color){ 211, 211, 211, 255 }
+#define COLOR_GAINSBORO (Color){ 220, 220, 220, 255 }
+#define COLOR_ROSYBROWN (Color){ 188, 143, 143, 255 }
+#define COLOR_LIGHTCORAL (Color){ 240, 128, 128, 255 }
+#define COLOR_INDIANRED (Color){ 205, 92, 92, 255 }
+#define COLOR_BROWN (Color){ 165, 42, 42, 255 }
+#define COLOR_FIREBRICK (Color){ 178, 34, 34, 255 }
+#define COLOR_MAROON (Color){ 128, 0, 0, 255 }
+#define COLOR_DARKRED (Color){ 139, 0, 0, 255 }
+#define COLOR_RED (Color){ 255, 0, 0, 255 }
+#define COLOR_MISTYROSE (Color){ 255, 228, 225, 255 }
+#define COLOR_SALMON (Color){ 250, 128, 114, 255 }
+#define COLOR_TOMATO (Color){ 255, 99, 71, 255 }
+#define COLOR_DARKSALMON (Color){ 233, 150, 122, 255 }
+#define COLOR_CORAL (Color){ 255, 127, 80, 255 }
+#define COLOR_ORANGERED (Color){ 255, 69, 0, 255 }
+#define COLOR_LIGHTSALMON (Color){ 255, 160, 122, 255 }
+#define COLOR_CRIMSON (Color){ 220, 20, 60, 255 }
+#define COLOR_PINK (Color){ 255, 192, 203, 255 }
+#define COLOR_LIGHTPINK (Color){ 255, 182, 193, 255 }
+
+// === ORANGES & BROWNS ===
+
+#define COLOR_SIENNA (Color){ 160, 82, 45, 255 }
+#define COLOR_CHOCOLATE (Color){ 210, 105, 30, 255 }
+#define COLOR_SADDLEBROWN (Color){ 139, 69, 19, 255 }
+#define COLOR_SANDYBROWN (Color){ 244, 164, 96, 255 }
+#define COLOR_PEACHPUFF (Color){ 255, 218, 185, 255 }
+#define COLOR_PERU (Color){ 205, 133, 63, 255 }
+#define COLOR_BISQUE (Color){ 255, 228, 196, 255 }
+#define COLOR_DARKORANGE (Color){ 255, 140, 0, 255 }
+#define COLOR_BURLYWOOD (Color){ 222, 184, 135, 255 }
+#define COLOR_ANTIQUEWHITE (Color){ 250, 235, 215, 255 }
+#define COLOR_TAN (Color){ 210, 180, 140, 255 }
+#define COLOR_NAVAJOWHITE (Color){ 255, 222, 173, 255 }
+#define COLOR_BLANCHEDALMOND (Color){ 255, 235, 205, 255 }
+
+// === YELLOWS & KHAKIS ===
+
+#define COLOR_PAPAYAWHIP (Color){ 255, 239, 213, 255 }
+#define COLOR_MOCCASIN (Color){ 255, 228, 181, 255 }
+#define COLOR_ORANGE (Color){ 255, 165, 0, 255 }
+#define COLOR_WHEAT (Color){ 245, 222, 179, 255 }
+#define COLOR_DARKGOLDENROD (Color){ 184, 134, 11, 255 }
+#define COLOR_GOLDENROD (Color){ 218, 165, 32, 255 }
+#define COLOR_CORNSILK (Color){ 255, 248, 220, 255 }
+#define COLOR_GOLD (Color){ 255, 215, 0, 255 }
+#define COLOR_LEMONCHIFFON (Color){ 255, 250, 205, 255 }
+#define COLOR_KHAKI (Color){ 240, 230, 140, 255 }
+#define COLOR_PALEGOLDENROD (Color){ 238, 232, 170, 255 }
+#define COLOR_DARKKHAKI (Color){ 189, 183, 107, 255 }
+#define COLOR_BEIGE (Color){ 245, 245, 220, 255 }
+#define COLOR_LIGHTYELLOW (Color){ 255, 255, 224, 255 }
+
